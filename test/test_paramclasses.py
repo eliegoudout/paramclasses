737d3aa73d1538b73d0a7fc2181afb2413989d34
Élie Goudout <eliegoudout@hotmail.com>
2024-12-27 23:01:58 +0100

    dev: assume currently no tests

---
+++ test/test_paramclasses.py
@@ -0,0 +1,9 @@
+"""Extensive testing should be added soon."""
+
+from paramclasses import ParamClass
+
+
+def test_trivial() -> None:
+    """Test trivial example."""
+
+    class A(ParamClass): ...
