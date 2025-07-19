41982620d6f659c2d9db070473b7b6ea4ac01407
Élie Goudout <eliegoudout@hotmail.com>
2025-07-19 23:48:21 +0200

    type: plugin: early structure for paramclass redefinition

--- mypy/plugin.py
+++ mypy/plugin.py
@@ -1,19 +1,69 @@
 """Mypy plugin for paramclasses."""
 
-from mypy.plugin import Plugin
+from functools import partial
+from collections.abc import Callable
 
+from mypy.plugin import ClassDefContext, FunctionContext, Plugin
+
+
+METAPARAMCLASS_FULLNAME = "paramclasses.paramclasses._MetaParamClass"
 
 class CustomPlugin(Plugin):
     """The plugin."""
 
-    def get_function_hook(self, fullname: str) -> type | None:
-        """Change signature of functions."""
-        if fullname == "paramclasses.paramclasses.foo":
-            return bool
-        return None
+    _paramclasses = set()  # Tracks all paramclasses founds during plugin life
+
+    def get_metaclass_hook(self, fullname) -> Callable[[ClassDefContext], None] | None:
+        """Update class definition when it uses metaclass."""
+        return paramclass_finder_hook
+
+    def get_base_class_hook(self, fullname) -> Callable[[ClassDefContext], None] | None:
+        """Update class definition when it uses base class."""
+        # print("get_base_class_hook", fullname,)
+        return paramclass_finder_hook
+
+    # def get_function_hook(self, fullname: str) -> Callable[[FunctionContext], Type] | None:
+    #     """Change signature of functions."""
+    #     if fullname.startswith("paramclasses."):
+    #         print(f"==== {fullname} ====")
+    #         return v
+    #     return None
+
+def paramclass_finder_hook(ctx):
+    """Stores paramclasses and applies logic function."""
+    mcs = ctx.cls.info.metaclass_type
+    if mcs is not None and mcs.type.fullname == METAPARAMCLASS_FULLNAME:
+        modify_paramclass_def(ctx.cls)
+
+
+def modify_paramclass_def(cls) -> None:
+    """"""
+    replace_protected_decorator_with_final(cls)
+    replace_protected_assignment_with_Final(cls)
+
+
+def replace_protected_decorator_with_final(cls) -> None:
+    print(f"Replaced {cls.info.fullname!r} protected decorators")
+
+def replace_protected_assignment_with_Final(cls) -> None:
+    print(f"Replaced {cls.info.fullname!r} protected assignments")
+
+
 
 
 def plugin(_version: str) -> Plugin:
     """Follow tuto."""
     # ignore version argument if the plugin works with all mypy versions.
     return CustomPlugin
+
+def v(func_ctx):
+    print(f"arg_types: {func_ctx.arg_types}")
+    print(f"arg_kinds: {func_ctx.arg_kinds}")
+    print(f"callee_arg_names: {func_ctx.callee_arg_names}")
+    print(f"arg_names: {func_ctx.arg_names}")
+    print(f"default_return_type: {func_ctx.default_return_type}")
+    print(f"args: {[[str(x) for x in arg] for arg in func_ctx.args]}")
+    print(f"context: {func_ctx.context}")
+    print(f"api: {func_ctx.api}")
+    breakpoint()
+    return func_ctx.default_return_type
--- test/paramclasses/test_typing.py
+++ test/paramclasses/test_typing.py
@@ -2,46 +2,71 @@
 
 import pytest
 
-from paramclasses import ParamClass, protected
+from paramclasses import RawParamClass, ParamClass, protected
 
 
 @pytest.mark.mypy_testing
-def test_typing():
-    """Test identified cases (no test for now)."""
+def test_a():
+    class A(ParamClass): ...
 
-    class A(ParamClass):
-        x1: int = 0
-        x2: int = protected(0)  # Should PASS
-        x3: int = protected(None)  # Should FAIL (None)
-        x4 = protected(0)
+    class B(RawParamClass): ...
 
-        @protected  # Should PASS
-        @property
-        def y(self) -> int:
-            return self.x1 + self.x2 + self.x4  # Should PASS
+    class C(int): ...
 
+    class D(A, C):
+        x: int = protected(6)
         @protected
-        def z(self) -> int:
-            return self.x1 + self.x2 + self.x4  # Should PASS
-
+        def y(self) -> None: ...
+        @protected
         @property
-        def t(self) -> int:
-            y: int = self.y  # Should PASS
-            z: int = self.z()  # Should PASS
-            return y + z
+        def z(self) -> None: ...
+        @protected
+        @protected
+        def t(self) -> None: ...
 
-    A(x1=None)  # Should FAIL (None)
-    A(0)  # Should FAIL (signature)
-    A(y=0)  # Should FAIL (signature)
-    A(x2=0)  # Should FAIL (Final)
-    a = A()
-    A.x1 = None  # Should FAIL (None)
-    a.x1 = None  # Should FAIL (None)
-    A.x2 = 0  # Should FAIL (Final)
-    a.x2 = 0  # Should FAIL (Final)
-    A.x4 = 0  # Should FAIL (Final)
-    a.x4 = 0  # Should FAIL (Final)
-    A.y = lambda: 0  # Should FAIL (final)
-    a.y = lambda: 0  # Should FAIL (read-only property)
-    A.z = 0  # Should FAIL (final)
-    a.z = 0  # Should FAIL (final)
+
+@pytest.mark.mypy_testing
+def test_b():
+    class A(ParamClass): ...
+
+
+# @pytest.mark.mypy_testing
+# def test_typing():
+#     """Test identified cases (no test for now)."""
+
+#     class A(ParamClass):
+#         x1: int = 0
+#         x2: int = protected(0)  # Should PASS
+#         x3: int = protected(None)  # Should FAIL (None)
+#         x4 = protected(0)
+
+#         @protected  # Should PASS
+#         @property
+#         def y(self) -> int:
+#             return self.x1 + self.x2 + self.x4  # Should PASS
+
+#         @protected
+#         def z(self) -> int:
+#             return self.x1 + self.x2 + self.x4  # Should PASS
+
+#         @property
+#         def t(self) -> int:
+#             y: int = self.y  # Should PASS
+#             z: int = self.z()  # Should PASS
+#             return y + z
+
+#     A(x1=None)  # Should FAIL (None)
+#     A(0)  # Should FAIL (signature)
+#     A(y=0)  # Should FAIL (signature)
+#     A(x2=0)  # Should FAIL (Final)
+#     a = A()
+#     A.x1 = None  # Should FAIL (None)
+#     a.x1 = None  # Should FAIL (None)
+#     A.x2 = 0  # Should FAIL (Final)
+#     a.x2 = 0  # Should FAIL (Final)
+#     A.x4 = 0  # Should FAIL (Final)
+#     a.x4 = 0  # Should FAIL (Final)
+#     A.y = lambda: 0  # Should FAIL (final)
+#     a.y = lambda: 0  # Should FAIL (read-only property)
+#     A.z = 0  # Should FAIL (final)
+#     a.z = 0  # Should FAIL (final)
